2d453a6470b7282af66f1f00e46b28d77267b039
WouterJansen <wouterjansen4@gmail.com>
2018-06-03 02:15:31 +0200

    improvement to wrong OS style.

--- app/window.js
+++ app/window.js
@@ -8,12 +8,13 @@ $(() => {
     var executablePath = app.getPath('downloads') + '\\gimp-2.8.22-setup.exe';
     var magnetURI = 'http://www.legittorrents.info/download.php?id=c691c817f7310ab3e7851576814dfc167bb52740&f=gimp-2.8.22-setup.exe.torrent';
     client.add(magnetURI, {path: 'C:/Users/Wouter Jansen/Downloads'}, onTorrent);
+
     function onButtonPress() {
         console.log(executablePath);
         shell.showItemInFolder(executablePath);
     }
 
-    document.querySelector('#install-button').addEventListener('click', onButtonPress)
+    document.querySelector('#install-button').addEventListener('click', onButtonPress);
 
     function onTorrent(torrent) {
         var interval = setInterval(function () {
@@ -29,24 +30,24 @@ $(() => {
             $('#progress-bar').attr('class', 'progress-bar progress-bar-striped');
             $('#install-container').css('visibility', 'visible');
             clearInterval(interval);
-            if(getOsVersion()){
+            if (getOsVersion()) {
                 $('#install-instructions').text('Please open the folder and double click the ISO file to mount it. Then you can start the Setup process.');
-            }else{
+            } else {
                 $('#install-instructions').text('Please use software such as 7ZIP or WinRar to open the ISO file you have downloaded and start the Setup executable to start the PR:BF2 installer.');
             }
         })
     }
 });
 
-function getOsVersion(){
+function getOsVersion() {
     var os = require('os');
     var release = os.release();
-    var splitRelease = release.split('.')
-    if(splitRelease[0] == '10' || splitRelease[0] == '8'){
-        console.log('mount version')
+    var splitRelease = release.split('.')/**/;
+    if (splitRelease[0] === '10' || splitRelease[0] === '8') {
+        console.log('mount version');
         return true;
-    }else{
-        console.log('none-mount version')
+    } else {
+        console.log('none-mount version');
         return false;
     }
 
