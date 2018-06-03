25fa10cfc94d0c5d04cc1a8af02af402c43c1bd3
WouterJansen <wouterjansen4@gmail.com>
2018-06-03 02:07:25 +0200

    Added OS detection. Added custom styling.

--- app/main.js
+++ app/main.js
@@ -1,16 +1,30 @@
-const {app, BrowserWindow} = require('electron')
+const {app, BrowserWindow} = require('electron');
+var path = require('path');
+var os = require('os');
 
 function createWindow() {
     win = new BrowserWindow({
         width: 700,
-        height: 600,
+        height: 350,
         autoHideMenuBar: true,
+        frame: true,
         darkTheme: true,
         resizable: false,
         icon: path.join(__dirname, 'assets/icons/png/64x64.png')
-    })
-    win.loadFile('./app/index.html')
+    });
+
+    win.on('ready-to-show', () => {
+        win.focus();
+        win.show();
+        win.webContents.openDevTools({ mode: 'undocked' });
+    });
+
+    if (os.platform() == 'win32') {
+        win.loadFile('./app/index.html')
+    } else {
+        win.loadFile('./app/wrongos.html')
+    }
 }
 
-app.on('ready', createWindow)
+app.on('ready', createWindow);
 
--- app/window.js
+++ app/window.js
@@ -2,12 +2,12 @@ $(() => {
     var WebTorrent = require('webtorrent');
     const remote = require('electron').remote;
     const shell = require('electron').shell;
+
     const app = remote.app;
     var client = new WebTorrent();
     var executablePath = app.getPath('downloads') + '\\gimp-2.8.22-setup.exe';
     var magnetURI = 'http://www.legittorrents.info/download.php?id=c691c817f7310ab3e7851576814dfc167bb52740&f=gimp-2.8.22-setup.exe.torrent';
     client.add(magnetURI, {path: 'C:/Users/Wouter Jansen/Downloads'}, onTorrent);
-
     function onButtonPress() {
         console.log(executablePath);
         shell.showItemInFolder(executablePath);
@@ -19,15 +19,35 @@ $(() => {
         var interval = setInterval(function () {
             $('#progress-bar').attr('aria-valuenow', torrent.progress * 100);
             $('#progress-bar').css('width', torrent.progress * 100 + '%');
-            $('#progress-bar').text(torrent.progress);
+            $('#progress-bar').text(Math.round(torrent.progress * 100) + '%');
         }, 1000);
 
         torrent.on('done', function () {
             $('#progress-bar').attr('aria-valuenow', 100);
             $('#progress-bar').css('width', 100 + '%');
             $('#progress-bar').text('Completed');
+            $('#progress-bar').attr('class', 'progress-bar progress-bar-striped');
             $('#install-container').css('visibility', 'visible');
             clearInterval(interval);
+            if(getOsVersion()){
+                $('#install-instructions').text('Please open the folder and double click the ISO file to mount it. Then you can start the Setup process.');
+            }else{
+                $('#install-instructions').text('Please use software such as 7ZIP or WinRar to open the ISO file you have downloaded and start the Setup executable to start the PR:BF2 installer.');
+            }
         })
     }
 });
+
+function getOsVersion(){
+    var os = require('os');
+    var release = os.release();
+    var splitRelease = release.split('.')
+    if(splitRelease[0] == '10' || splitRelease[0] == '8'){
+        console.log('mount version')
+        return true;
+    }else{
+        console.log('none-mount version')
+        return false;
+    }
+
+}
