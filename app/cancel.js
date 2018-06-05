e5a270431f990c975b244ff1c394c1a3d5951e34
Wouter J <wouterjansen4@gmail.com>
2018-06-05 22:16:38 +0200

    Added cancel options. Added pause and resume options. Added test version check.

---
+++ app/cancel.js
@@ -0,0 +1,77 @@
+$(() => {
+    const remote = require('electron').remote;
+    const app = remote.app;
+    const fs = require('fs');
+    const path = require('path');
+    const shell = require('electron').shell;
+    let spawn = require("child_process").spawn, child;
+    const win = remote.getCurrentWindow();
+    const configPath = path.join(app.getPath('userData'), 'config.json');
+    let isoPath = 'none';
+    const versionInformationURL = 'https://www.realitymod.com/version/versiontest.json';
+
+    function getDownloadStoragePath() {
+        return JSON.parse(fs.readFileSync(configPath)).downloadStoragePath;
+    }
+
+    function onMinimizeButtonPress() {
+        win.minimize();
+    }
+
+    function onWebsiteLinkPress() {
+        shell.openExternal('https://www.realitymod.com')
+    }
+
+    function onGithubLinkPress() {
+        shell.openExternal('https://github.com/WouterJansen/PRBF2Download')
+    }
+
+    function onCloseButtonPress() {
+        app.quit();
+        win.close();
+
+    }
+
+    function getTorrentFileName(handleData) {
+        $.ajax({
+            url: versionInformationURL,
+            success:function(data) {
+                handleData(data.torrent_filename);
+            }
+        });
+    }
+
+    function onContinueButtonPress() {
+        win.loadFile('./app/download.html')
+
+    }
+
+    function onCancelButtonPress() {
+        app.quit();
+        win.close();
+
+    }
+
+    function onCancelAndRemoveButtonPress() {
+        try{
+            fs.unlinkSync(isoPath);
+        }catch(err){
+            console.log(err)
+        }
+        fs.unlinkSync(configPath);
+        app.quit();
+        win.close();
+    }
+
+    getTorrentFileName(function(torrent_filename){
+        isoPath = path.join(getDownloadStoragePath(), torrent_filename)
+    });
+
+    document.querySelector('#cancel-button').addEventListener('click', onCancelButtonPress);
+    document.querySelector('#cancel-remove-button').addEventListener('click', onCancelAndRemoveButtonPress);
+    document.querySelector('#continue-button').addEventListener('click', onContinueButtonPress);
+    document.querySelector('#close-button').addEventListener('click', onCloseButtonPress);
+    document.querySelector('#minimize-button').addEventListener('click', onMinimizeButtonPress);
+    document.querySelector('#website-link').addEventListener('click', onWebsiteLinkPress);
+    document.querySelector('#github-link').addEventListener('click', onGithubLinkPress);
+});
--- app/download.js
+++ app/download.js
@@ -14,7 +14,10 @@ $(() => {
     let installerName = 'none';
     let setupPath = 'none';
     let isoPath = 'none';
-
+    const versionInformationURL = 'https://www.realitymod.com/version/versiontest.json';
+    let paused = false;
+    let completed = false;
+    const configPath = path.join(app.getPath('userData'), 'config.json');
     function findMountedDrive() {
         let found = false;
         for (let i = 65; i <= 90; i++) {
@@ -30,7 +33,7 @@ $(() => {
 
     function getVersionBig(handleData) {
         $.ajax({
-            url: "https://www.realitymod.com/version/version.json",
+            url: versionInformationURL,
             success:function(data) {
                 handleData(data.version_big);
             }
@@ -39,7 +42,7 @@ $(() => {
 
     function getTorrentURL(handleData) {
         $.ajax({
-            url: "https://www.realitymod.com/version/version.json",
+            url: versionInformationURL,
             success:function(data) {
                 handleData(data.torrent_url);
             }
@@ -48,7 +51,7 @@ $(() => {
 
     function getTorrentFileName(handleData) {
         $.ajax({
-            url: "https://www.realitymod.com/version/version.json",
+            url: versionInformationURL,
             success:function(data) {
                 handleData(data.torrent_filename);
             }
@@ -57,7 +60,7 @@ $(() => {
 
     function getTorrentSetupName(handleData) {
         $.ajax({
-            url: "https://www.realitymod.com/version/version.json",
+            url: versionInformationURL,
             success:function(data) {
                 handleData(data.torrent_setupname);
             }
@@ -65,25 +68,31 @@ $(() => {
     }
 
     function getDownloadStoragePath() {
-        return JSON.parse(fs.readFileSync(path.join(app.getPath('userData'), 'config.json'))).downloadStoragePath;
+        return JSON.parse(fs.readFileSync(configPath)).downloadStoragePath;
     }
 
     function onInstallButtonPress() {
-        if (getOsVersion()) {
-            setupPath = findMountedDrive();
-            if (setupPath === 'none') {
-                child = spawn("powershell.exe", ["-Command", "Mount-DiskImage -ImagePath \"" + isoPath + "\""]);
-                setTimeout(function () {
+        if(!completed){
+            win.setProgressBar(0, {mode: "normal"});
+            if(!paused){client.remove(client.torrents[0]);}
+            win.loadFile('./app/cancel.html')
+        }else {
+            if (getOsVersion()) {
+                setupPath = findMountedDrive();
+                if (setupPath === 'none') {
+                    child = spawn("powershell.exe", ["-Command", "Mount-DiskImage -ImagePath \"" + isoPath + "\""]);
+                    setTimeout(function () {
+                        setupPath = findMountedDrive();
+                        child = spawn(setupPath);
+                    }, 3000);
+                } else {
                     setupPath = findMountedDrive();
                     child = spawn(setupPath);
-                }, 3000);
+                }
+                $('#install-instructions').text('Once the installation is complete you can remove this utility and delete the Installer ISO file.');
             } else {
-                setupPath = findMountedDrive();
-                child = spawn(setupPath);
+                shell.showItemInFolder(isoPath);
             }
-            $('#install-instructions').text('Once the installation is complete you can remove this utility.');
-        } else {
-            shell.showItemInFolder(isoPath);
         }
     }
 
@@ -106,7 +115,26 @@ $(() => {
     function onCloseButtonPress() {
         app.quit();
         win.close();
+    }
 
+    function onTorrentContinueButtonPress() {
+        $('#torrent-pause').css('color', 'white');
+        $('#torrent-start').css('color', '#5a5757');
+        getTorrentURL(function(torrent_url){
+            client.add(torrent_url, {path: getDownloadStoragePath()}, onTorrent);
+        });
+        paused = false;
+        $('#progress-information-time').text('Verifying Files...');
+    }
+    function onTorrentPauseButtonPress() {
+        win.setProgressBar(client.progress, {mode: "paused"});
+        $('#torrent-start').css('color','white');
+        $('#torrent-pause').css('color','#5a5757');
+        client.remove(client.torrents[0]);
+        $('#progress-information-size').text(' ');
+        $('#progress-information-time').text('Paused...');
+        $('#progress-information-speed').text(' ');
+        paused = true;
     }
 
     function getOsVersion() {
@@ -118,26 +146,31 @@ $(() => {
 
     function onTorrent(torrent) {
         let interval = setInterval(function () {
-            win.setProgressBar(torrent.progress, {mode: "normal"});
-            $('#progress-bar').attr('aria-valuenow', torrent.progress * 100);
-            $('#progress-bar').css('width', torrent.progress * 100 + '%');
-            $('#progress-bar').text(Math.round(torrent.progress * 100) + '%');
-            if (Math.round(torrent.progress * 100) === 100) {
-                $('#progress-information-size').text(' ');
-                $('#progress-information-time').text('Verifying Files...');
-                $('#progress-information-speed').text(' ');
-            } else {
-                $('#progress-information-size').text(prettyBytes(torrent.progress * torrent.length) + '/' + prettyBytes(torrent.length));
-                $('#progress-information-time').text(humanizeDuration(torrent.timeRemaining, {
-                    round: true,
-                    largest: 2
-                }) + ' remaining');
-                $('#progress-information-speed').text(prettyBytes(torrent.downloadSpeed) + '/s');
+            if(!paused) {
+                win.setProgressBar(client.progress, {mode: "normal"});
+                $('#progress-bar').attr('aria-valuenow', client.progress * 100);
+                $('#progress-bar').css('width', client.progress * 100 + '%');
+                $('#progress-bar').text(Math.round(client.progress * 100) + '%');
+                if (Math.round(client.progress * 100) === 100) {
+                    $('#progress-information-size').text(' ');
+                    $('#progress-information-time').text('Verifying Files...');
+                    $('#progress-information-speed').text(' ');
+                } else {
+                    $('#progress-information-size').text(prettyBytes(client.progress * torrent.length) + '/' + prettyBytes(torrent.length));
+                    $('#progress-information-time').text(humanizeDuration(torrent.timeRemaining, {
+                        round: true,
+                        largest: 2
+                    }) + ' remaining');
+                    $('#progress-information-speed').text(prettyBytes(client.downloadSpeed) + '/s');
+                }
+            }else{
+                clearInterval(interval);
             }
-
         }, 1000);
 
         torrent.on('done', function () {
+            win.setProgressBar(1, {mode: "normal"});
+            completed = true;
             app.setAppUserModelId("PRBF2-Download-Assistant");
             let notification = {
                 title: "PR:BF2 Download Assistant",
@@ -149,20 +182,22 @@ $(() => {
             myNotification.onclick = () => {
                 win.show();
             };
-            win.setProgressBar(1, {mode: "none"});
             $('#progress-bar').attr('aria-valuenow', 100);
             $('#progress-bar').css('width', 100 + '%');
             $('#progress-bar').text('Completed');
             $('#progress-bar').attr('class', 'progress-bar progress-bar-striped');
             $('#progress-information').css('visibility', 'hidden');
             $('#install-container').css('visibility', 'visible');
+            $('#torrent-pause').css('visibility', 'hidden');
+            $('#torrent-start').css('visibility', 'hidden');
             clearInterval(interval);
             torrent.destroy();
             if (getOsVersion()) {
                 $('#install-instructions').text('');
-                $('#install-button').text('Start Installer');
+                $('#install-button').text('Start installer');
             } else {
-                $('#install-instructions').text('Please use software such as 7ZIP or WinRar to open the ISO file you have downloaded. Then can you start the installer(Setup.exe).');
+                $('#install-button').text('Open downloads folder');
+                $('#install-instructions').text('Please use software such as 7ZIP or WinRar to open the ISO file you have downloaded. Then can you start the installer (' + installerName + ').');
             }
         })
     }
@@ -173,6 +208,9 @@ $(() => {
     document.querySelector('#website-link').addEventListener('click', onWebsiteLinkPress);
     document.querySelector('#github-link').addEventListener('click', onGithubLinkPress);
     document.querySelector('#prbf2-manual-link').addEventListener('click', onPRManualLinkPress);
+    document.querySelector('#torrent-start').addEventListener('click', onTorrentContinueButtonPress);
+    document.querySelector('#torrent-pause').addEventListener('click', onTorrentPauseButtonPress);
+
     getVersionBig(function(version){
         $('#prbf2-version').text(version);
     });
@@ -185,7 +223,6 @@ $(() => {
     getTorrentSetupName(function(torrent_setupname){
         installerName = torrent_setupname;
     });
-
 
 
 
